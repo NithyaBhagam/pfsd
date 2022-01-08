a009c3435e7dec9b4e4c8b8e65b4b36424707e23
NithyaBhagam <87516036+NithyaBhagam@users.noreply.github.com>
2022-01-08 08:27:28 +0530

    Add files via upload

---
+++ inheritence.py
@@ -0,0 +1,14 @@
+class Parent:
+    def function1(self):
+        print("this is function one")
+
+class  Child(Parent):
+    def function2(selfself,a):
+        print("this is function two")
+        print(a)
+    b=30
+
+Y=Child()
+Y.function1()
+Y.function2(10)
+print(Y.b)
